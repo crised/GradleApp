1554252ccf3a41b084eec777bbc5cf35a9fe8da5
crised <crised@gmail.com>
2015-11-05 19:27:30 -0300

    first commit

---
+++ app/src/test/java/telematic/cl/gradleapp/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package telematic.cl.gradleapp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
